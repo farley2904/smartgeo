ecaad65dbb6ee74c55ee272374ec4a3608feec75
Yarik <vashchishinya@gmail.com>
2020-11-21 19:11:29 +0200

    add Home.vue with logo

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+
+        <div>
+            <jet-application-logo class="block h-12 w-auto" />
+        </div>
+    <!-- </app-layout> -->
+</template>
+
+<script>
+    import JetApplicationLogo from '@/Jetstream/ApplicationLogo'
+
+    export default {
+        components: {
+            JetApplicationLogo,
+        },
+    }
+</script>
+
