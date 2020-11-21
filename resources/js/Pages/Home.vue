5bc98fe6245f4fecf7a94385eb5f31bf4c0385a7
Yarik <vashchishinya@gmail.com>
2020-11-21 19:19:43 +0200

    edited bg and logo in navigation

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="bg-gray-800">
+<nav class="bg-white">
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
     <div class="relative flex items-center justify-between h-16">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -29,7 +29,7 @@
       <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
         <div class="flex-shrink-0 flex items-center">
           <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
-          <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+            <jet-application-logo class="hidden lg:block h-8 w-auto" />
         </div>
         <div class="hidden sm:block sm:ml-6">
           <div class="flex space-x-4">
